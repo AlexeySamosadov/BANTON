799a4b6ac989048757cfd68c6732942c45a761ea
Самосадов Алексей Вадимович <asamosadov@phoenixit.ru>
2022-01-24 11:33:43 +0300

    First steps

---
+++ config.js
@@ -0,0 +1,5 @@
+ const PORT = 3000
+const TOKEN = '5199205767:AAHycJU33xhZ_k4QknEMZpZx9f5fC8Gwc9A'
+
+module.exports =  {PORT , TOKEN}
+ 
---
+++ keyboards.js
@@ -0,0 +1,14 @@
+// const  Markup  = require('telegraf')
+const M = require('telegraf-markup4')
+
+//  const getMainMenu = () =>{
+//     return Markup.keyboard([
+//         ['Мои задачи', 'Добавить задачу'],
+//         ['Смотивируй меня']
+//     ]).resize().extra()
+// }
+
+// M.keyboard.reply([['Кнопка 1'], ['Кнопка 2', 'Кнопка 3']])
+ 
+
+module.exports = {M}
