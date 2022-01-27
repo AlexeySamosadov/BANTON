58902de86c9401dbdc00f3a9a3f54cc0bccf6445
Самосадов Алексей Вадимович <asamosadov@phoenixit.ru>
2022-01-27 13:11:35 +0300

    Сделал запросы на телеграм ТОН

--- scenes/advertisiment.js
+++ scenes/advertisiment.js
@@ -1,9 +1,22 @@
 const { Markup, Composer, Scenes } = require('telegraf')
 const axios = require('axios')
 
+// Запросы на TON чтобы получить информацию по конкрентной транзаки
+const WALLET_ADDRESS = 'EQC37faknSAl9Uc1ccqcbA9jpBSXSIR9j8yncIDtHr41eUvc'
+const TRANSACTION_TIME = '24794687000001'
+const TRANSACTION_HASH = 'k3fg13W4L4tFG2/tjLHCWC6qXAJfnXwAb1W342X9XVY'
+
+
+// Тестовые не работают:
+// const WALLET_ADDRESS = 'EQC-8zBcr2d12_9tlR04qPlPUJsYtJ7kFgDLjLidqL3S0HzL'
+// const TRANSACTION_TIME = '25000784000001'
+// const TRANSACTION_HASH = 'Wl7k4QNdTWkBzonDzuy4npu3/1q/ghr1IYjiYqdWEuk='
+
+
+
 const getMainWalletTransactions = async () => {
   try {
-    return await axios.get('https://api.ton.sh/getTransactions?address=EQC37faknSAl9Uc1ccqcbA9jpBSXSIR9j8yncIDtHr41eUvc')
+    return await axios.get(`https://api.ton.sh/getTransactions?address=${WALLET_ADDRESS}&lt=${TRANSACTION_TIME}&hash=${TRANSACTION_HASH}=&limit=1`)
   } catch (error) {
     console.error(error)
   }
