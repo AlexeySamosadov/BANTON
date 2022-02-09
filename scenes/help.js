edfc60ccf2ada17e28a67986be3fb61d9afcba32
Самосадов Алексей Вадимович <asamosadov@phoenixit.ru>
2022-02-10 00:40:47 +0300

    message update

--- scenes/help.js
+++ scenes/help.js
@@ -6,6 +6,20 @@ const KeyBoards = require('../buttons/buttons')
 const startStep = new Composer()
 startStep.on('text', async (ctx) => {
 	try {
+		const messengerName = String(ctx.update?.message?.from?.first_name)		
+		await ctx.replyWithHTML(
+			  `<b>${messengerName}</b>, для получения помощи напишите сюда в чат и мы обязательно вам ответим`
+ 		)
+
+		return ctx.wizard.next()
+	} catch (e) {
+		console.log(e)
+	}
+})
+
+const secondStep = new Composer()
+secondStep.on('text', async (ctx) => {
+	try {
 		const messengerName = String(ctx.update?.message?.from?.first_name)
 		const Keyboard = Markup.keyboard([
 			['Пополнить баланс', 'Вывод средств'],
@@ -13,18 +27,31 @@ startStep.on('text', async (ctx) => {
 		])
 			.oneTime()
 			.resize()
-		
-		await ctx.replyWithHTML(
-			  `<b>${messengerName}</b>, для получения помощи напишите <a href="https://t.me/AlexeySamosadov">сюда</a> `, 
-			  Keyboard
-		)
+		const mesenger = ctx.update?.message?.from // 
+		const message = ctx.update?.message?.text 		 
 
+		await ctx.telegram.sendMessage(192816064,`Пользователь @${mesenger?.username} написал в техпотдержку со словами: 
+			
+			"${message}"
+
+			Его данные :
+			Имя ${mesenger?.first_name},
+			Фамилия ${mesenger?.last_name},
+			id: ${mesenger?.id}
+			Бот или человек: ${mesenger?.is_bot ? 'Это бот' : ' Это Человек'},
+			Открытый или закрытый аккаунт:  ${ctx.update?.message?.chat?.type}
+		`)
+
+		await ctx.replyWithHTML(
+			`<b>${messengerName}</b>, мы получили ваше сообщение, администратор вскоре вам ответит`,
+			Keyboard
+	  )
 		return ctx.scene.leave()
 	} catch (e) {
 		console.log(e)
 	}
 })
 
-const resumeScene = new Scenes.WizardScene('helpWizard', startStep)
+const resumeScene = new Scenes.WizardScene('helpWizard', startStep, secondStep)
 
 module.exports = resumeScene
